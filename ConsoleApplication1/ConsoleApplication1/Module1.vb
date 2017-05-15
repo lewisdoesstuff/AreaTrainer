1b4088348c083ef189c22aa40965be3228de545d
DeathByKorea <deathbykorea@gmail.com>
2017-05-15 15:34:21 +0100

    The last one was a lie
    - Added a password check
    - Fixed some stuff
    - Broke some stuff
    - Fixed that

--- ConsoleApplication1/ConsoleApplication1/Module1.vb
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -2,6 +2,9 @@
 Imports System.IO
 Module Module1
     Dim score As Integer = 0
+    Dim loggedin As Boolean = False
+    Dim name As String
+
     Sub Main()
         Randomize()
         Console.ForegroundColor = ConsoleColor.White
@@ -88,29 +91,35 @@ Module Module1
         Console.WriteLine("Welcome to the Area Trainer")
         Console.WriteLine("1. Play")
         Console.WriteLine("2. Register")
-
-        Console.WriteLine("2. Quit")
+        Console.WriteLine("3. Save and Quit")
         Dim temp = Console.ReadLine()
         If temp = 1 Then
             names()
         ElseIf temp = 2 Then
             Register()
+        ElseIf temp = 3 Then
+            If loggedin Then
+                My.Computer.FileSystem.WriteAllText("login\Users" & name & "\game.csv", score, False)
+                Console.WriteLine("Your score for this session was" & score)
+                Environment.Exit(0.1)
+            Else
+                Environment.Exit(0.0)
+            End If
         End If
     End Sub
     Sub names()
         Dim password As String = Nothing
-        Dim name As String
         Console.WriteLine("Enter Username")
         name = Console.ReadLine
         Dim csv As String = Nothing
-        If File.Exists("login\Users\" & name & ".csv") Then
+        If File.Exists("login\Users\" & name & "\" & name & " .csv") Then
         Else
             Console.WriteLine("Username does not exist")
             Threading.Thread.Sleep(2500)
             Console.Clear()
             Main()
         End If
-        Dim pass As String = My.Computer.FileSystem.ReadAllText("login\Users\" & name & ".csv")
+        Dim pass As String = My.Computer.FileSystem.ReadAllText("login\Users\" & name & "\" & name & " .csv")
         Console.WriteLine("Enter your password")
         Dim key As ConsoleKeyInfo
         While key.Key <> ConsoleKey.Enter
@@ -148,7 +157,7 @@ Module Module1
 
                 Console.Clear()
             Next
-
+            loggedin = True
             Play()
         Else
             Console.WriteLine("Incorrect Password")
@@ -161,20 +170,35 @@ Module Module1
     Sub Register()
         Console.WriteLine("Enter a Username")
         Dim user As String = Console.ReadLine()
-        If File.Exists("login\Users\" & user & ".csv") Then
+        If File.Exists("login\Users\" & user & "\" & user & " .csv") Then
             Console.WriteLine("User name already exists")
             Threading.Thread.Sleep(2000)
             Register()
         End If
-        Console.WriteLine("Enter a Password")
-        Console.ReadKey(True)
-        Console.Write("*")
-        Dim pass As String = Console.ReadLine()
-        My.Computer.FileSystem.WriteAllText("login\Users\" & user & ".csv", pass, False)
+        Console.WriteLine("Enter your password")
+        Dim password As String = Nothing
+        Dim key As ConsoleKeyInfo
+        While key.Key <> ConsoleKey.Enter
+            key = Console.ReadKey(True)
+            If key.Key <> ConsoleKey.Backspace And key.Key <> ConsoleKey.Enter Then
+                password += key.KeyChar
+                Console.Write("*")
+            ElseIf key.Key = ConsoleKey.Backspace Then
+                key = Console.ReadKey(False)
+                key = Console.ReadKey(True)
+            End If
+        End While
+        If password = "password" Then
+            Console.WriteLine("Don't be absurd, Use a better password!")
+        Else
+        End If
+        System.IO.Directory.CreateDirectory("login\Users\" & user & "\")
+        My.Computer.FileSystem.WriteAllText("login\Users\" & user & "\" & user & " .csv", password, False)
         Console.WriteLine("You have registered!")
         Threading.Thread.Sleep(2000)
         Main()
     End Sub
+
     Sub Play()
         Console.WriteLine("Choose a shape")
         Console.WriteLine("1. ")
@@ -209,6 +233,7 @@ Module Module1
             circle()
         End If
     End Sub
+
     Sub square()
         Console.Clear()
         Console.WriteLine("Your score is: " & score)
@@ -222,7 +247,7 @@ Module Module1
         Console.WriteLine("Y = " & y)
         Dim x = CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
         Console.WriteLine("x = " & x)
-        Dim area = x * y
+        Dim area As String = x * y
         Dim a, b, c, d As Double
         a = area + CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
         b = area - CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
@@ -238,14 +263,16 @@ Module Module1
         ElseIf ans = 4 Then
             d = area
         End If
-        Console.WriteLine("A=" & a & " B=" & b & " C=" & c & " D=" & d)
+        Console.WriteLine("A=" & a & " B=" & b & " C=" & c & " D=" & d & " Q: Go to menu")
         Console.WriteLine("Enter Answer")
         ans = Console.ReadLine()
         If ans = area Then
             Console.WriteLine("That is correct!")
             score += 1
             Threading.Thread.Sleep(1000)
-            circle()
+            square()
+        ElseIf ans = "q" Or ans = "Q" Then
+            Main()
         Else
             Console.WriteLine("That is incorrect, The correct answer was" & area)
             Threading.Thread.Sleep(1000)
@@ -268,7 +295,7 @@ Module Module1
         Console.WriteLine("Y = " & y)
         Dim x = CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
         Console.WriteLine("x = " & x)
-        Dim area = (x * y) / 2
+        Dim area As String = (x * y) / 2
         Dim a, b, c, d As Double
         a = area + CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
         b = area - CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
@@ -284,14 +311,14 @@ Module Module1
         ElseIf ans = 4 Then
             d = area
         End If
-        Console.WriteLine("A=" & a & " B=" & b & " C=" & c & " D=" & d)
+        Console.WriteLine("A=" & a & " B=" & b & " C=" & c & " D=" & d & " Q: Go to menu")
         Console.WriteLine("Enter Answer")
         ans = Console.ReadLine()
         If ans = area Then
             Console.WriteLine("That is correct!")
             score += 1
             Threading.Thread.Sleep(1000)
-            circle()
+            triangle()
         Else
             Console.WriteLine("That is incorrect, The correct answer was" & area)
             Threading.Thread.Sleep(1000)
@@ -309,7 +336,7 @@ Module Module1
         Console.WriteLine(" *          *")
         Console.WriteLine("  *        *")
         Console.WriteLine("     *  *")
-        Dim area = CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
+        Dim area As String = CInt(Math.Floor((10 - 1 + 1) * Rnd())) + 1
         Console.WriteLine("Radius" & area)
 
         area = Math.Round((area * area) * Math.PI)
@@ -328,7 +355,7 @@ Module Module1
         ElseIf ans = 4 Then
             d = area
         End If
-        Console.WriteLine("A=" & a & " B=" & b & " C=" & c & " D=" & d)
+        Console.WriteLine("A=" & a & " B=" & b & " C=" & c & " D=" & d & " Q: Go to menu")
         Console.WriteLine("Enter Answer")
         ans = Console.ReadLine()
         If ans = area Then
